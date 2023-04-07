36982073222a48b74ba6c61cb8e501b3cd117bce
Ashish Ranjan <aranjan@maira.io>
2023-04-07 14:24:58 -0700

    "bugfix release"

--- Casks/mpl.rb
+++ Casks/mpl.rb
@@ -1,1 +1,13 @@
 cask "mpl" do
+  version "0.9.0"
+  sha256 "76d8026eb7ec2d892d70fb18d04efeb86e59f649fa0e93250094984be3fb74cb"
+
+  url "https://github.com/maira-io/mpl-release/download/v#{version}/mpl-#{version}-darwin-universal.zip"
+  name "Mpl repl"
+  desc "Mpl repl integrated with Maira integration CLIs"
+  homepage "https://maira.io"
+
+  binary "mpl-#{version}-darwin-universal/mpl"
+
+  # No zap stanza required
+end
